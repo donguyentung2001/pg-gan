2b0792477953a5e74d257aaaf8bc61b08ca61915
Tung Do <tdo@haverford.edu>
2021-06-18 12:23:19 -0400

    fix chs

--- prep_data/prep_vcf.sh
+++ prep_data/prep_vcf.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-
-SUFFIX=.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
-
+​
+INFOLDER=/bigdata/smathieson/pg-gan/1000g/ALL/
+OUTFOLDER=/bigdata/smathieson/pg-gan/1000g/VCF_Tung/
+SUFFIX=.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes(2).vcf.gz
+​
 # for each set of population(s)
 for POP in CHB
 do
 
   # for each chromosome
-  for CHROM in `seq 1 22`
-  do
-      echo "bcftools view -S ${POP}_gan.txt --min-ac 1:minor -m2 -M2 -v snps -Oz -o ${POP}.chr${CHROM}${SUFFIX} ALL.chr${CHROM}${SUFFIX}"
-      bcftools view -S ${POP}_gan.txt --min-ac 1:minor -m2 -M2 -v snps -Oz -o ${POP}.chr${CHROM}${SUFFIX} ALL.chr${CHROM}${SUFFIX}
-  done
+  #for CHROM in `seq 1 22`
+  #do
+  #    echo "bcftools view -S ${POP}_gan.txt --min-ac 1:minor -m2 -M2 -v snps -Oz -o ${POP}.chr${CHROM}${SUFFIX} ALL.chr${CHROM}${SUFFIX}"
+  #    bcftools view -S ${POP}_gan.txt --min-ac 1:minor -m2 -M2 -v snps -Oz -o ${POP}.chr${CHROM}${SUFFIX} ALL.chr${CHROM}${SUFFIX}
+  #done
 
   # then merge into one vcf
   echo "bcftools concat -f ${POP}_filelist.txt -Oz -o ${POP}${SUFFIX}"
