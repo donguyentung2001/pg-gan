3a1046338bf52eba631987948008961081aa6e37
Tung Do <tdo@haverford.edu>
2021-07-13 13:24:52 -0400

    remove tensorflow

--- pca_visual.py
+++ pca_visual.py
@@ -2,9 +2,7 @@ import math
 import numpy as np 
 import random
 import sys
-import tensorflow as tf
 from scipy.optimize import basinhopping 
-from VAE import decoder, encoder, CVAE, train_step, reparameterize_only
 import real_data_random
 import util 
 from sklearn.decomposition import PCA
